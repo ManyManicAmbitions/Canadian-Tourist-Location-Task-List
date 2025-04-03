ba4e00a1ee8d6447e619e56fe82cc8bce9777fbb
RohitPurandhara <rohitp25@hotmail.com>
2025-04-03 16:36:50 -0400

    Edited the explore page with javascript

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
 
 function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
+    // Declare variables
+  var input, filter, ul, li, i, txtValue;
+  //What the user inputs
+  input = document.getElementById("myInput");
+  //Filters the result to uppercase letters to eliminate case-sensitive results
   filter = input.value.toUpperCase();
+  //Variable for unordered list
   ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+  //Variable for list item
+  li = ul.getElementsByTagName("li");
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+//Getting text from the list item
+      txtValue = li[i].textContent || li[i].innerText; 
+//If statement to check if the user input is inside a certain list item
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          //If there is a matching item, that item stays visible
+          li[i].style.display = "";
+      } else {
+        //Items that do not match will be hidden
+          li[i].style.display = "none";
+      }
   }
 }
 
-// function myFunction() {
-//     var input, filter, ul, li, a, i, txtValue;
-//     input = document.getElementById("myInput");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myUL");
-//     li = ul.getElementsByTagName("li");
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("a")[0];
-//         txtValue = a.textContent || a.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
