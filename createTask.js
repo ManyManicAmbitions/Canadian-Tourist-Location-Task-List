c544ecb7f220e0418c5fdd5388c2db7e3a3b0484
RohitPurandhara <rohitp25@hotmail.com>
2025-04-17 20:09:40 -0400

    changed script js to include createtask functions

--- createTask.js
+++ createTask.js
@@ -1,16 +1,1 @@
-const taskForm = document.getElementById("taskForm");
-const confirmationDiv = document.getElementById("confirmed");
 
-    taskForm.addEventListener("submit", createTask);
-
-    function createTask(event){
-        event.preventDefault();
-
-        const title = document.getElementById("title").value;
-        const description = document.getElementById("description").value;
-        const location = document.getElementById("location").value;
-        const priority = document.getElementById("priority").value;
-
-        document.getElementById("confirmation").textContent = "Task created";
-
-    }
